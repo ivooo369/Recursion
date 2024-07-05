af20b687296b5dbd474f8fdf88efef17adcddbc9
ivooo369 <ivoivelinov@abv.bg>
2024-07-06 01:30:34 +0300

    Add merge sort solution

---
+++ mergeSort.js
@@ -0,0 +1,38 @@
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const mid = Math.floor(array.length / 2);
+  const left = array.slice(0, mid);
+  const right = array.slice(mid);
+
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  return merge(sortedLeft, sortedRight);
+}
+
+function merge(left, right) {
+  const result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  result.push(...left.slice(i));
+  result.push(...right.slice(j));
+
+  return result;
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
